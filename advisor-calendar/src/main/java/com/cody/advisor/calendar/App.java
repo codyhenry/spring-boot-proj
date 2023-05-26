2d5b2542f27bcd32f523fa5b94a091ee063e08cb
codyhenry <codyhenry527@gmail.com>
2023-05-26 15:32:05 -0400

    init commit

---
+++ advisor-calendar/src/main/java/com/cody/advisor/calendar/App.java
@@ -0,0 +1,13 @@
+package com.cody.advisor.calendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
