b71cba3ee3aa9cebd1c73d805766111725157e45
codyhenry <codyhenry527@gmail.com>
2023-06-04 15:26:54 -0400

    added production only features

--- advisor-calendar/src/main/java/com/cody/advisor/calendar/config/DataLoader.java
+++ advisor-calendar/src/main/java/com/cody/advisor/calendar/config/DataLoader.java
@@ -1,10 +1,13 @@
 package com.cody.advisor.calendar.config;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 //Can use this to initialize data in a local database, runs after everything else is up
-//@Component
+//determine when to run this code - set to not run in dev
+@Profile("!dev")
+@Component
 public class DataLoader implements CommandLineRunner {
 
   @Override
